01a7e4c8d7ce0fa4fb4925bea037974740e2c634
Bruno Wunsch <bruno_wunsch@hotmail.com>
2025-09-16 21:46:25 -0300

    Fixed backend routes

--- lib/routes/backend.ts
+++ lib/routes/backend.ts
@@ -4,13 +4,13 @@ export const BACKEND_URI  = "http://localhost:8000";
 
 
 //~ === === === AUTH ENDPOINTS === === === ~//
-export const LOGIN_API_URL  = `${BACKEND_URI}/api/login`;
-export const SIGNIN_API_URL = `${BACKEND_URI}/api/signin`;
+export const LOGIN_API_URL  = `${BACKEND_URI}/api/v1/login`;
+export const SIGNIN_API_URL = `${BACKEND_URI}/api/v1/signin`;
 
 
 //? === === === USER ENDPOINTS === === === ?//
-export const USER_API_URL = `${BACKEND_URI}/api/user`;
+export const USER_API_URL = `${BACKEND_URI}/api/v1/user`;
 
 
 //? === === === PRODUCT ENDPOINTS === === === ?//
-export const PRODUCT_API_URL = `${BACKEND_URI}/api/product`;
+export const PRODUCT_API_URL = `${BACKEND_URI}/api/v1/product`;
