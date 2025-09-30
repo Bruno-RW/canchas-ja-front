f4a51a757cda4ceae0e444a5789b83c0b9b60db2
Bruno Wunsch <bruno_wunsch@hotmail.com>
2025-09-30 20:19:37 -0300

    Fixed footer position

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import Kicker from "@/components/footer/components/Kicker";
 
 const Footer = () => {
   return (
-    <footer className="sticky bottom-0 z-50 flex flex-col w-full px-5 pt-10 pb-5 gap-y-8 bg-brand-bg-secondary">
+    <footer className="z-50 flex flex-col w-full px-5 pt-10 pb-5 gap-y-8 bg-brand-bg-secondary">
       <Columns />
       <Separator className="bg-gray-200" />
       <Kicker />
