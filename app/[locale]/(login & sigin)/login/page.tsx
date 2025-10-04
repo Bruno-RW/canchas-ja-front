61693eed445df1947ec1bba216cad4038ce4d364
Bruno Wunsch <bruno_wunsch@hotmail.com>
2025-10-04 15:38:48 -0300

    Fixed login page and sigin text

--- app/[locale]/(login & sigin)/login/page.tsx
+++ app/[locale]/(login & sigin)/login/page.tsx
@@ -3,15 +3,34 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
+import axios from "axios";
+
+import { LOGIN_API_URL } from "@/lib/routes/backend";
 
 import useSession from "@/hooks/useSession";
 
 import LoginForm from "@/components/forms/LoginForm";
 
 const LoginPage = () => {
-  const t = useTranslations("LoginPage");
-  const { user } = useSession();
+  const t = useTranslations("Page.Login.LoginPage");
+  const { user, login } = useSession();
   const router = useRouter();
+
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const response = await axios.post(LOGIN_API_URL, { email, password });
+
+      if (response.data.user) {
+        login(response.data.user);
+        router.replace("/");
+      }
+
+      console.error(response.data.error);
+
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
 
   useEffect(() => {
     if (user.isLogin) router.replace("/");
@@ -19,8 +38,8 @@ const LoginPage = () => {
 
   return (
     <section className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3 py-6 px-5 border rounded-md border-gray-300 dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-none">
-      <h1 className="mb-5">{t("Title")}</h1>
-      <LoginForm />
+      <h1 className="text-5xl font-bold mb-5">{t("Title")}</h1>
+      <LoginForm onSubmit={handleLogin} />
     </section>
   );
 };
--- app/[locale]/(login & sigin)/signin/page.tsx
+++ app/[locale]/(login & sigin)/signin/page.tsx
@@ -9,7 +9,7 @@ import useSession from "@/hooks/useSession";
 import SigninForm from "@/components/forms/SigninForm";
 
 const SigninPage = () => {
-  const t = useTranslations("SigninPage");
+  const t = useTranslations("Page.SignIn.SigninPage");
   const { user } = useSession();
   const router = useRouter();
 
@@ -19,7 +19,7 @@ const SigninPage = () => {
 
   return (
     <section className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3 py-6 px-5 border rounded-md border-gray-300 dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-none">
-      <h1 className="mb-5">{t("Title")}</h1>
+      <h1 className="text-5xl font-bold mb-5">{t("Title")}</h1>
       <SigninForm />
     </section>
   );
