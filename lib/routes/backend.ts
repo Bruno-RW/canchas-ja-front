66f746dc7229490af662fdcf638f10fc03c4a3cc
Bruno Wunsch <bruno_wunsch@hotmail.com>
2025-10-04 15:38:27 -0300

    Added more routes

--- lib/routes/backend.ts
+++ lib/routes/backend.ts
@@ -10,7 +10,11 @@ export const SIGNIN_API_URL = `${BACKEND_URI}/api/v1/signin`;
 
 //? === === === USER ENDPOINTS === === === ?//
 export const USER_API_URL = `${BACKEND_URI}/api/v1/user`;
+export const USER_FAVORITES_API_URL = `${USER_API_URL}/favorites`;
 
 
 //? === === === PRODUCT ENDPOINTS === === === ?//
 export const PRODUCT_API_URL = `${BACKEND_URI}/api/v1/product`;
+export const PRODUCT_SPECIAL_DISCOUNT_API_URL = `${PRODUCT_API_URL}/special_discounts`;
+export const PRODUCT_BEST_RATED_API_URL = `${PRODUCT_API_URL}/best_rated`;
+export const PRODUCT_NEAR_YOU_API_URL = `${PRODUCT_API_URL}/near_you`;
